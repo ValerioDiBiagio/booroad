794af6253cf085581604c55d68234b0be2a0025c
Marco-Rocco <marcorocco28@icloud.com>
2025-05-16 18:11:56 +0200

    feat: add card for each trip in homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,32 @@
+import trips from "../data/trips";
+
+
 const Homepage = () => {
+
 
     return (
         <>
-            <div>Sono Homepage</div>
+
+
+            {trips.length ? trips.map((trip) => (
+                <div class="card mb-3">
+                    <div class="card-header">
+                        {trip.destination}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">With {trip.guide}</h5>
+                        <p class="card-text"> {trip.startDate} - {trip.endDate} </p>
+                        <a href="#" class="btn btn-primary">See details</a>
+                    </div>
+                </div>
+            )) : <div>trips were not found</div>}
+
+
+
+
         </>
     )
 };
+
 
 export default Homepage;

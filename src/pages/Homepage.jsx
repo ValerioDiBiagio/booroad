7a937ec65008327c29b1c6f6348a093d77675d6c
MegaEmme <mikmusco@gmail.com>
2025-05-16 18:19:58 +0200

    feat: fix card rendering in Homepage by correcting className usage and displaying full guide name

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,14 +9,14 @@ const Homepage = () => {
 
 
             {trips.length ? trips.map((trip) => (
-                <div class="card mb-3">
-                    <div class="card-header">
+                <div className="card mb-3" key={trip.id}>
+                    <div className="card-header">
                         {trip.destination}
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">With {trip.guide}</h5>
-                        <p class="card-text"> {trip.startDate} - {trip.endDate} </p>
-                        <a href="#" class="btn btn-primary">See details</a>
+                    <div className="card-body">
+                        <h5 className="card-title">With {trip.guide.name} {trip.guide.surname}</h5>
+                        <p className="card-text"> {trip.startDate} - {trip.endDate} </p>
+                        <a href="#" className="btn btn-primary">See details</a>
                     </div>
                 </div>
             )) : <div>trips were not found</div>}

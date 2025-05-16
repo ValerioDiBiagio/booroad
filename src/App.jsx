9f66a0a9b1125036a11889546c2cc55b58e76f9d
MegaEmme <mikmusco@gmail.com>
2025-05-16 16:46:31 +0200

    fix: resolve merge conflict in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import Homepage from './pages/Homepage';
@@ -12,11 +12,5 @@ export default function App() {
                 </Route>
             </Routes>
         </BrowserRouter>
-    );
-=======
-export default function App() {
-  return (
-    <></>
-  )
->>>>>>> cd9f797ae75abe23f5e1d29f1203647e4122f05c
-};
+    )
+}

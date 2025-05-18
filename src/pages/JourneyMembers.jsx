c061c449532a3060e3eba86d836502a14246c4d1
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-19 01:19:42 +0200

    feat: update JourneyMembers component to display traveller details in a card layout

--- src/pages/JourneyMembers.jsx
+++ src/pages/JourneyMembers.jsx
@@ -1,7 +1,23 @@
+import trips from "../data/trips";
+
 const JourneyMembers = () => {
+
     return (
         <>
-            <div>Sono Membro viaggio</div>
+            {trips.map((trip) =>
+                trip.travellers.map((traveller) => (
+                    <div key={traveller.CF} className="card mb-3">
+                        <div className="card-header">
+                            <h2>{traveller.name} {traveller.surname}</h2>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Codice Fiscale: {traveller.CF}</p>
+                            <p className="card-text">Numero di telefono: {traveller.phone}</p>
+                            <p className="card-text">Email: {traveller.email}</p>
+                        </div>
+                    </div>
+                ))
+            )}
         </>
     )
 };

c54f03db8cc145ecce6f29dd96c9ac448d4c6e5e
MegaEmme <mikmusco@gmail.com>
2025-05-19 16:32:20 +0200

    fix: remove fixed positioning and top margin from header and jumbotron styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className="bg-warning py-3 mb-4 position-fixed w-100 z-3">
+        <header className="bg-warning py-3 mb-4 z-3">
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="logo border border-white rounded-5 bg-warning-subtle">
                     <a href="/"><img src="../src/public/logo_2.png" alt="logo" className="p-2" /></a>

ba3b1b6c6e8e01248321ccb5380c46f17ed8ed85
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-19 11:33:56 +0200

    feat: add conditional rendering for members in JourneyMembers component

--- src/pages/JourneyMembers.jsx
+++ src/pages/JourneyMembers.jsx
@@ -4,18 +4,22 @@ const JourneyMembers = () => {
 
     return (
         <>
-            {members.map((member) => (
-                <div className="card mb-3" key={member.id}>
-                    <div className="card-header">
-                        <h5>{member.name} {member.surname}</h5>
+            {members.length ? (
+                members.map((member) => (
+                    <div className="card mb-3" key={member.id}>
+                        <div className="card-header">
+                            <h5>{member.name} {member.surname}</h5>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">Email: {member.email} </p>
+                            <p className="card-text">Numero di Telefono: {member.phone} </p>
+                            <p className="card-text">Codice Fiscale: {member.CF} </p>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text">Email: {member.email} </p>
-                        <p className="card-text">Numero di Telefono: {member.phone} </p>
-                        <p className="card-text">Codice Fiscale: {member.CF} </p>
-                    </div>
-                </div>
-            ))}
+                ))
+            ) : (
+                <div>members were not found</div>
+            )}
         </>
     )
 };

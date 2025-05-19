d8ec430a2127bf430e623c53827485943b105031
MegaEmme <mikmusco@gmail.com>
2025-05-19 13:01:38 +0200

    feat: update logo and enhance styles in Header and JourneyDetail components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,10 @@ const Header = () => {
     return (
         <header className="bg-warning py-3 mb-4">
             <div className="container d-flex justify-content-between align-items-center">
-                <a href="/journeys-detail" className="logo text-decoration-none text-light bg-success p-3 rounded-5 fs-5">Booroad</a>
+                <div className="logo border border-white rounded-5 bg-warning-subtle">
+                    <a href="/journeys-detail"><img src="../src/public/logo_2.png" alt="logo" className="p-2" /></a>
+                </div>
                 <ul className="d-flex list-unstyled gap-3">
-                    {/* <li>
-                        <NavLink to='/'>Home Page</NavLink>
-                    </li> */}
                     <li>
                         <NavLink to='/journeys-detail'>Viaggi</NavLink>
                     </li>

eac40d83c130105a1ea10c6d6a7692fb432b8ef1
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-19 11:59:10 +0200

    feat: fix css card

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,16 +10,15 @@ const Homepage = () => {
 
             <Jumbotron />
             {trips.length ? trips.map((trip) => (
-                <div className="container card mb-3 flex-row justify-content-around" key={trip.id}>
-                    <img className="w-25" src={trip.image} alt={trip.destination} />
+                <div className="bg-warning-subtle container card mb-3 flex-row justify-content-around" key={trip.id}>
+                    <img className="w-25 m-3 rounded-2" src={trip.image} alt={trip.destination} />
                     <div className="w-50">
                         <h3 className="p-2 m-4 text-center">{trip.destination}</h3>
                         <p className="card-title text-center"> <strong>Guida:</strong> {trip.guide.name} {trip.guide.surname}</p>
                         <p className="card-text text-center"> <strong>Data inizio:</strong> {trip.startDate} - <strong>Data fine:</strong> {trip.endDate}</p>
                     </div>
                     <div className="card-body d-flex align-items-center">
-
-                        <a href={`/journey-details/${trip.id}`} className="btn btn-primary ">Dettagli del viaggio</a>
+                        <a href={`/journey-details/${trip.id}`} className="btn btn-primary p-2">Dettagli del viaggio</a>
                     </div>
                 </div>
             )) : <div>trips were not found</div>}

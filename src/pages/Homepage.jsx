8a1be328784bbeff4a695687c8df901c8eeb5f27
MegaEmme <mikmusco@gmail.com>
2025-05-19 11:48:02 +0200

    feat: update Header component to use a link for the logo; fix CSS syntax in index.css; clean up Homepage component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,6 @@ const Homepage = () => {
 
     return (
         <>
-
             <Jumbotron />
             {trips.length ? trips.map((trip) => (
                 <div className="container card mb-3 flex-row justify-content-around" key={trip.id}>

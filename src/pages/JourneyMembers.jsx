5c538b9a806e3ee444775113aa91783413a7999b
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-19 15:40:03 +0200

    feat: add icon

--- src/pages/JourneyMembers.jsx
+++ src/pages/JourneyMembers.jsx
@@ -8,12 +8,12 @@ const JourneyMembers = () => {
                 members.map((member) => (
                     <div className="card mb-3 bg-warning" key={member.id}>
                         <div className="card-header">
-                            <h5 className="p-2">{member.name} {member.surname}</h5>
+                            <h5 className="p-2"> <i class="fa-regular fa-user"></i> {member.name} {member.surname}</h5>
                         </div>
                         <div className="card-body bg-warning-subtle">
-                            <p className="card-text"><strong>Email: </strong>{member.email} </p>
-                            <p className="card-text"><strong>Numero di Telefono: </strong>{member.phone} </p>
-                            <p className="card-text"><strong>Codice Fiscale: </strong>{member.CF} </p>
+                            <p className="card-text"> <i class="fa-solid fa-envelope"></i> <strong>Email: </strong>{member.email} </p>
+                            <p className="card-text"> <i class="fa-solid fa-phone"></i> <strong>Numero di Telefono: </strong>{member.phone} </p>
+                            <p className="card-text"> <i class="fa-solid fa-id-card"></i> <strong>Codice Fiscale: </strong>{member.CF} </p>
                         </div>
                     </div>
                 ))

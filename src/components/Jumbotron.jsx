1a8ad85fdc8ef395b0782755b51e3b2d48e3c4e0
MegaEmme <mikmusco@gmail.com>
2025-05-19 17:10:11 +0200

    fix: add Pacifico font and update Jumbotron heading styling

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,7 +1,7 @@
 const Jumbotron = () => {
     return (
         <>
-            <div className="container shadow jumbotron mb-4 text-center fs-5 p-4">BOOROAD</div>
+            <div className="container shadow jumbotron mb-4 text-center fs-5 p-4"><h2 className="jumbotron-heading text-warning">Booroad</h2></div>
         </>
     )
 };

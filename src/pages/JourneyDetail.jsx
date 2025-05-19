5b72b2febe50a070298331e3e24c4cf078daf5bc
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-19 16:07:34 +0200

    feat: fix card

--- src/pages/JourneyDetail.jsx
+++ src/pages/JourneyDetail.jsx
@@ -11,12 +11,14 @@ export default function JourneyDetail() {
     <>
       {trip ? (
         <>
-          <h1>Dettagli del Viaggio a {trip.destination}</h1>
-          <div>
-            <p><strong>Data Inizio: </strong>{trip.startDate}</p>
-            <p><strong>Data Fine: </strong>{trip.endDate}</p>
-            <p><strong>Guida: </strong><Link className="text-decoration-none" to={`/journey-guide-details/${trip.guide.id}`}>{trip.guide.name} {trip.guide.surname}</Link></p>
-            <h3 className='mb-4'><strong>Viaggiatori: </strong></h3>
+          <div className='bg-warning-subtle'>
+            <h1>Dettagli del Viaggio a {trip.destination}:</h1>
+            <div>
+              <p><strong> <i className="p-2 fa-solid fa-plane-departure"></i> Data Inizio: </strong>{trip.startDate}</p>
+              <p><strong> <i className="p-2 fa-solid fa-plane-arrival"></i> Data Fine: </strong>{trip.endDate}</p>
+              <p><strong>Guida: </strong><Link className="text-decoration-none" to={`/journey-guide-details/${trip.guide.id}`}>{trip.guide.name} {trip.guide.surname}</Link></p>
+            </div>
+            <h2 className='mb-4'><strong>Viaggiatori: </strong></h2>
             <ul>
               {trip.travellers.map((traveller) => {
                 const headingId = `heading-${traveller.id}`;
@@ -27,7 +29,7 @@ export default function JourneyDetail() {
                     <div className="accordion-item">
                       <h2 className="accordion-header" id={headingId}>
                         <button
-                          className="accordion-button collapsed"
+                          className="accordion-button collapsed  bg-warning"
                           type="button"
                           data-bs-toggle="collapse"
                           data-bs-target={`#${collapseId}`}
@@ -43,7 +45,7 @@ export default function JourneyDetail() {
                         aria-labelledby={headingId}
                         data-bs-parent={`#${parentId}`}
                       >
-                        <div className="accordion-body">
+                        <div className="accordion-body bg-warning-subtle">
                           <ul className='list-unstyled'>
                             <li><strong><i class="fa-solid fa-phone"></i> Numero di telefono: </strong>{traveller.phone}</li>
                             <li><i class="fa-solid fa-envelope"></i> <strong>Email: </strong>{traveller.email}</li>
